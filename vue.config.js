a7de5cfbc343cd23f69e549c91978534b0d6fe1f
uioasdzxc0789 <617378601@qq.com>
2022-06-26 09:45:07 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+// vue脚手架项目默认的配置文件名
+// webpack+node环境-导出配置对象
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    host: '127.0.0.1',
+    port: 3001
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '黑马头条'
+        return args
+      })
+  }
 })
